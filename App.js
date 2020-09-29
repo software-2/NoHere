5fdae273e471dd93ab23604416f2069d5740d327
software-2 <software-2@users.noreply.github.com>
2020-09-29 02:23:18 -0700

    Add in custom font framework

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
+import Constants from 'expo-constants';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import { AppLoading } from "expo";
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  SecularOne_400Regular,
+  OpenSans_400Regular
+} from "@expo-google-fonts/dev";
+
+function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    SecularOne_400Regular
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.headText}>Stop using @here</Text>
+        <StatusBar style="auto" />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+    backgroundColor: 'green'
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
+  },
+  headText: {
+    textAlign: 'center',
+    fontSize: 60,
+    fontFamily: "SecularOne_400Regular"
+  },
+
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
   },
 });
+
+
+export default App
